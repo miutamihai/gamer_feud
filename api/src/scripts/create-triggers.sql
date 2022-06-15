da41a6307277100b6f524b77ec807db3ba38079a
miutamihai <miuta.mihai@gmail.com>
2022-06-15 15:44:04 +0300

    [master] Delete reviews on game delition trigger

--- api/src/scripts/create-triggers.sql
+++ api/src/scripts/create-triggers.sql
@@ -8,3 +8,11 @@ begin
     delete from user_comments where comment_id in (select comment_id from comments where game_id = old.id);
     delete from comments where game_id = old.id;
 end;
+
+create or replace trigger delete_reviews
+    before delete
+    on games
+    for each row
+begin
+    delete from reviews where game_id = old.id;
+end;
