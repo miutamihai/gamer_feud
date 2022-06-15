7011a6a51702a00b3c4aceeb1702602c2fe835b8
miutamihai <miuta.mihai@gmail.com>
2022-06-15 10:14:04 +0300

    [master] Setup express app

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,18 @@
 const {config} = require('dotenv')
 const {create_db} = require('./create-db')
 const {create_tables} = require('./create-tables')
+const express = require('express')
 
 config()
+
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
 
 create_db()
     .then(() => console.log('Database created'))
     .then(() => create_tables())
     .then(() => console.log('Tables created'))
-    .then(() => process.exit(0))
+    .then(() => app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`)))
